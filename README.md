351cac8719346d2c50cd0e59fbbb028e209a669a
Adilson Perinei <zumiki@gmail.com>
2022-02-28 16:10:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# S3UploadToolSAM
+
+S3 Upload Tool SAM Application
+
+Deployment command (requires SAM cli)
+> sam deploy --config-env <dev>/<test>/<prod>
---
+++ copyFile/app.py
@@ -0,0 +1,37 @@
+import boto3
+import os
+from urllib.parse import unquote
+
+s3 = boto3.resource('s3',verify=True,region_name=os.environ['AWS_REGION'])
+TargetBucket = os.environ.get('TargetBucket')
+TargetBucketPrefix = os.environ.get('TargetBucketPrefix')
+
+def lambda_handler(event, context):
+    print('copy file started')
+    print(event)
+    for record in event['Records']:
+        source_bucket = record['s3']['bucket']['name']
+        source_key_durty = record['s3']['object']['key']
+        source_key = unquote(source_key_durty)
+    print("source bucket")
+    print(source_bucket)
+    print("source key")
+    print(source_key)
+    nickname = source_key.split('/')[2]
+    file_name = source_key.split('/')[3]
+    print("relailer name")
+    print(nickname)
+    print("file name")
+    print(file_name)
+    copy_source = {
+    'Bucket': source_bucket,
+    'Key': source_key
+    }
+    bucket = s3.Bucket(TargetBucket)
+    bucket.copy(copy_source, TargetBucketPrefix + "/" + nickname + "/" + file_name)
+    print("copy file complete")
+
+
+
+
+
